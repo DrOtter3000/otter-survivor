45ba44cd1d6da32155013bd3ea6b7bb13e8fca70
DrOtter3000 <info@darknet-online.com>
2024-07-06 17:21:16 +0200

    juice up scratch attack

--- Scenes/Attacks/scratch_attack.gd
+++ Scenes/Attacks/scratch_attack.gd
@@ -4,6 +4,10 @@ extends Area2D
 var damage: int = 1
 
 
+func _ready() -> void:
+	rotation_degrees = randi_range(-90, 90)
+
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("enemy"):
 		body.take_damage(damage)
